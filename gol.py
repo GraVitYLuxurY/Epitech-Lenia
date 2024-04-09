c6b46d1f60b0d78be8bd10aee5e70818ab70f755
0xsharkboy <achille.bitaud@gmail.com>
2024-04-09 18:11:38 +0200

    🔧 FIX | check rules

--- gol.py
+++ gol.py
@@ -1,11 +1,13 @@
 import sys
+import os
+import time
 
 cell_char = '#'
 check_around = [(0, 1), (1, 0), (1, 1), (1, -1), (0, -1), (-1, 0), (-1, -1), (-1, 1)]
 
 def init_grid(file):
     with open(file) as f:
-        return [[i for i in list(line)] for line in f]
+        return [[i for i in line.strip()] for line in f]
 
 def empty_grid(grid):
     for i in range(len(grid)):
@@ -14,33 +16,37 @@ def empty_grid(grid):
                 return False
     return True
 
-def can_live(cell):
+def can_live(cell, i, j):
     cells_around = 0
     for pos in check_around:
         try:
-            if cell[pos[0]][pos[1]] == cell_char:
+            if cell[i + pos[0]][j + pos[1]] == cell_char:
                 cells_around += 1
         except:
-            pass
-    if (cells_around < 2 or cells_around > 3) and cell == cell_char:
-        return '.'
-    elif cells_around == 3 and cell == '.':
-        return cell_char
-    elif cells_around == 2 and cell == cell_char:
-        return cell_char
+            continue
+    if (cell[i][j] == cell_char):
+        if cells_around < 2 or cells_around > 3:
+            return '.'
+        else:
+            return cell_char
     else:
-        return '.'
+        if cells_around == 3:
+            return cell_char
+        else:
+            return '.'
+
 
 def print_grid(grid):
     for i in range(len(grid)):
         for j in range(len(grid[i])):
             print(grid[i][j], end='')
+        print()
 
 def game_of_life(grid):
     new_grid = grid
     for i in range(len(grid)):
         for j in range(len(grid[i])):
-            new_grid[i][j] = can_live(grid[i][j])
+            new_grid[i][j] = can_live(grid, i, j)
     return new_grid
 
 def main(av):
@@ -51,6 +57,9 @@ def main(av):
         while not empty_grid(grid):
             print_grid(grid)
             grid = game_of_life(grid)
+            time.sleep(1)
+            os.system("clear")
+        print_grid(grid)
 
 if __name__ == '__main__':
     main(sys.argv)
